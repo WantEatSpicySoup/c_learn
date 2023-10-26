03b96e2b7b139f1d8f14a1abcd7257b8620d8b36
WantEatSpicySoup <carrot310215@163.com>
2023-10-26 23:30:48 +0800

    提交初始指针的代码

--- c_10_25/c_10_25/test.c
+++ c_10_25/c_10_25/test.c
@@ -2,7 +2,8 @@
 
 #include <stdio.h>
 
-int main()
+// 通过数组元素地址来遍历数组
+int main1()
 {
 	int arr[10] = {1,2,3,4,5,6,7,8,9,10};
 	int* pa = arr;
@@ -13,3 +14,30 @@ int main()
 
 	return 0;
 } 
+
+// 验证p+i和&arr[i]的地址相同
+int main2()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int size = sizeof(arr) / sizeof(arr[0]);
+	int* p = arr;
+	int i = 0;
+	for (i = 0; i < size; i++)
+	{
+		printf("arr[%d] = %p == %p\n", i, p+i, &arr[i]);
+	}
+
+	return 0;
+}
+
+// 定义和使用多级指针
+int main()
+{
+	int a = 10;
+	int* p = &a;
+	int** pp = &p;
+	int*** ppp = &pp;
+	printf("%d", ***ppp);
+
+	return 0;
+}
