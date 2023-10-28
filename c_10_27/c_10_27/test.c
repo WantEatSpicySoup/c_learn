eb077d2415be934e4b138ee43bfa9b17f69fc471
WantEatSpicySoup <carrot310215@163.com>
2023-10-28 22:57:04 +0800

    提交数组练习代码

--- c_10_27/c_10_27/test.c
+++ c_10_27/c_10_27/test.c
@@ -17,7 +17,6 @@ int main1()
 		printf("%d ", arr[i]);
 	}
 
-
 	return 0;
 }
 
@@ -31,7 +30,7 @@ void print_arr(int* arr, int size)
 	}
 }
 
-int main()
+int main2()
 {
 	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
 	int size = sizeof(arr) / sizeof(arr[0]);
@@ -56,6 +55,135 @@ int main3()
 		end--;
 	}
 	printf("%s", str);
+
+	return 0;
+}
+
+// 求Sn=a+aa+aaa+aaaa+aaaaa的前5项之和，其中a是一个数字
+int main4()
+{
+	int a = 0;
+	scanf("%d", &a);
+	int sum = 0;
+	int tmp = 0;
+	int i = 0;
+	for (i = 0; i < 5; i++)
+	{
+		tmp = 0;
+		int j = 0;
+		for (j = 0; j <= i; j++)
+		{
+			tmp *= 10;
+			tmp += a;
+		}
+		sum += tmp;
+	}
+	printf("%d\n", sum);
+	 
+	return 0;
+}
+
+#include <assert.h>
+
+// 模拟实现strlen()函数
+int my_strlen(const char* str)
+{
+	assert(str);
+	int count = 0;
+	while (*str)
+	{
+		count++;
+		str++;
+	}
+	return count;
+}
+
+int main5()
+{
+	char* str = "abcdefg";
+	printf("%d\n", my_strlen(str));
+
+	return 0;
+}
+
+// 输出0～100000之间的所有“水仙花数”
+int get_num(int val)
+{
+	if (val == 0)
+	{
+		return 1;
+	}
+	int count = 0;
+	while (val > 0)
+	{
+		count++;
+		val /= 10;
+	}
+	return count;
+}
+
+#include <math.h>
+
+int main6()
+{
+	int i = 0;
+	for (i = 0; i <= 100000; i++)
+	{
+		int count = get_num(i);
+		int res = 0;
+		int tmp = i;
+		while(tmp > 0)
+		{
+			res += (int)pow(tmp % 10, count);
+			tmp /= 10;
+		}
+		if (res == i)
+		{
+			printf("%d ", res);
+		}
+	}
+	return 0;
+}
+
+// 打印菱形
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	// 输出上部分
+	int i = 0;
+	for (i = 0; i < n / 2 + 1; i++)
+	{
+		// 输出空格
+		int j = 0;
+		for (j = 0; j < n / 2 - i; j++)
+		{
+			printf(" ");
+		}
+		// 输出字符
+		for (j = 0; j <= i * 2; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	// 输出下部分
+	for (i = 0; i < n / 2; i++)
+	{
+		// 输出空格
+		int j = 0;
+		for (j = 0; j <= i; j++)
+		{
+			printf(" ");
+		}
+		// 输出字符
+		for (j = 0; j <= n/2-i; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
 
 	return 0;
 }
